30343e11c8ca9e8468670792b58e000e6ca33788
aznhe21 <aznhe21.mc@gmail.com>
2023-01-30 22:14:54 +0900

    feat(isdb): サンプルコマンドのcountを追加

---
+++ crates/isdb/examples/count.rs
@@ -0,0 +1,124 @@
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::BufReader;
+use std::rc::Rc;
+
+use isdb::Pid;
+
+#[derive(Default)]
+struct Count {
+    input: u64,
+    continuity_error: u64,
+    scrambled: u64,
+}
+
+struct Filter {
+    input: u64,
+    format_error: u64,
+    transport_error: u64,
+    counts: isdb::pid::PidTable<Count>,
+}
+
+impl Default for Filter {
+    fn default() -> Filter {
+        Filter {
+            input: 0,
+            format_error: 0,
+            transport_error: 0,
+            counts: isdb::pid::PidTable::from_fn(|_| Count::default()),
+        }
+    }
+}
+
+impl isdb::demux::Filter for Filter {
+    fn on_pes_packet(&mut self, _: Pid, _: &[u8]) {}
+    fn on_psi_section(&mut self, _: Pid, _: &isdb::psi::PsiSection) {}
+
+    fn on_transport_error(&mut self) {
+        self.input += 1;
+        self.transport_error += 1;
+    }
+
+    fn on_format_error(&mut self) {
+        self.input += 1;
+        self.format_error += 1;
+    }
+
+    fn on_packet(&mut self, packet: &isdb::Packet) -> Option<isdb::demux::PacketType> {
+        self.input += 1;
+
+        let mut count = &mut self.counts[packet.pid()];
+        count.input += 1;
+        if packet.is_scrambled() {
+            count.scrambled += 1;
+        }
+
+        // PESの方が処理が軽い
+        Some(isdb::demux::PacketType::Pes)
+    }
+
+    fn on_discontinued(&mut self, pid: Pid) {
+        self.counts[pid].continuity_error += 1;
+    }
+}
+
+const HELP: &str = "\
+パケットを数えるコマンド
+
+USAGE:
+  count [PATH]
+
+FLAGS:
+  -h, --help このヘルプを表示する
+
+ARGS:
+  <PATH>     パケットを数えるTSファイルのパス
+";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = pico_args::Arguments::from_env();
+    if args.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let path: std::path::PathBuf = args.free_from_str()?;
+
+    env_logger::init();
+
+    let f = File::open(path)?;
+    let f = BufReader::with_capacity(188 * 1024, f);
+
+    let filter = Rc::new(RefCell::new(Filter::default()));
+    let mut demuxer = isdb::demux::Demuxer::new(filter.clone());
+
+    for packet in isdb::Packet::iter(f) {
+        demuxer.handle(&packet?);
+    }
+
+    let filter = filter.borrow();
+    let continuity_error = filter
+        .counts
+        .iter()
+        .map(|c| c.continuity_error)
+        .sum::<u64>();
+    let scrambled = filter.counts.iter().map(|c| c.scrambled).sum::<u64>();
+
+    println!("Input Packets   : {:9}", filter.input);
+    println!("Format Error    : {:9}", filter.format_error);
+    println!("Transport Error : {:9}", filter.transport_error);
+    println!("Dropped         : {:9}", continuity_error);
+    println!("Scrambled       : {:9}", scrambled);
+    println!();
+    println!(" PID :     Input   Dropped Scrambled");
+    for (pid, count) in filter.counts.iter().enumerate() {
+        if count.input != 0 {
+            println!(
+                "{:04X} : {:9} {:9} {:9}",
+                pid, count.input, count.continuity_error, count.scrambled,
+            );
+        }
+    }
+
+    Ok(())
+}
