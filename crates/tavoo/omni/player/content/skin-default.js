cc7a7315e2f12f0c54b8b6754127b891a1be9f72
aznhe21 <aznhe21.mc@gmail.com>
2023-06-05 20:37:35 +0900

    refactor(tavoo): 映像領域用要素と制御用オブジェクトに分離

--- crates/tavoo/omni/player/content/skin-default.js
+++ crates/tavoo/omni/player/content/skin-default.js
@@ -1,6 +1,6 @@
-export class Skin extends HTMLElement {
-  #player;
+import { gController } from "tavoo://player/content/controller.js";
 
+export class Skin extends HTMLElement {
   constructor() {
     super();
 
@@ -9,49 +9,70 @@ export class Skin extends HTMLElement {
       <root xmlns="http://www.w3.org/1999/xhtml">
         <link rel="stylesheet" href="tavoo://player/skin/default.css" />
 
-        <tavoo-player id="player"></tavoo-player>
+        <tavoo-screen id="screen"></tavoo-screen>
       </root>
     `, "application/xml");
 
     const shadow = this.attachShadow({ mode: "open" });
     shadow.append(...doc.documentElement.children);
+  }
 
-    this.#player = shadow.getElementById("player");
+  connectedCallback() {
+    gController.addEventListener("source", this);
+    gController.addEventListener("volume", this);
+    gController.addEventListener("rate-range", this);
+    gController.addEventListener("duration", this);
+    gController.addEventListener("state", this);
+    gController.addEventListener("position", this);
+    gController.addEventListener("seek-completed", this);
+    gController.addEventListener("rate", this);
+    gController.addEventListener("resize", this);
+    gController.addEventListener("dual-mono-mode", this);
+    gController.addEventListener("services", this);
+    gController.addEventListener("service", this);
+    gController.addEventListener("event", this);
+    gController.addEventListener("service-changed", this);
+    gController.addEventListener("stream-changed", this);
+    gController.addEventListener("caption", this);
+    gController.addEventListener("superimpose", this);
+    gController.addEventListener("timestamp", this);
+  }
 
-    this.#player.addEventListener("source", this);
-    this.#player.addEventListener("volume", this);
-    this.#player.addEventListener("rate-range", this);
-    this.#player.addEventListener("duration", this);
-    this.#player.addEventListener("state", this);
-    this.#player.addEventListener("position", this);
-    this.#player.addEventListener("seek-completed", this);
-    this.#player.addEventListener("rate", this);
-    this.#player.addEventListener("resize", this);
-    this.#player.addEventListener("dual-mono-mode", this);
-    this.#player.addEventListener("services", this);
-    this.#player.addEventListener("service", this);
-    this.#player.addEventListener("event", this);
-    this.#player.addEventListener("service-changed", this);
-    this.#player.addEventListener("stream-changed", this);
-    this.#player.addEventListener("caption", this);
-    this.#player.addEventListener("superimpose", this);
-    this.#player.addEventListener("timestamp", this);
+  disconnectedCallback() {
+    gController.removeEventListener("source", this);
+    gController.removeEventListener("volume", this);
+    gController.removeEventListener("rate-range", this);
+    gController.removeEventListener("duration", this);
+    gController.removeEventListener("state", this);
+    gController.removeEventListener("position", this);
+    gController.removeEventListener("seek-completed", this);
+    gController.removeEventListener("rate", this);
+    gController.removeEventListener("resize", this);
+    gController.removeEventListener("dual-mono-mode", this);
+    gController.removeEventListener("services", this);
+    gController.removeEventListener("service", this);
+    gController.removeEventListener("event", this);
+    gController.removeEventListener("service-changed", this);
+    gController.removeEventListener("stream-changed", this);
+    gController.removeEventListener("caption", this);
+    gController.removeEventListener("superimpose", this);
+    gController.removeEventListener("timestamp", this);
   }
 
   handleEvent(e) {
     switch (e.target) {
-      case this.#player:
+      case gController:
         switch (e.type) {
           case "source":
-            console.log(`ファイル：${this.#player.source}`);
+            console.log(`ファイル：${gController.source}`);
             break;
 
           case "volume":
-            console.log(`音量：${this.#player.volume}`);
+            console.log(`音量：${gController.volume}`);
             break;
 
           case "rate-range": {
-            const { slowest, fastest } = this.#player.playbackRateRange;
+            const { slowest, fastest } = gController.playbackRateRange;
             console.log(`速度範囲：${slowest}..=${fastest}`);
             break;
           }
@@ -61,11 +82,11 @@ export class Skin extends HTMLElement {
             break;
 
           case "state":
-            console.log(`再生状態：${this.#player.state}`);
+            console.log(`再生状態：${gController.state}`);
             break;
 
           case "position":
-            console.log(`再生位置：${this.#player.currentTime}`);
+            console.log(`再生位置：${gController.currentTime}`);
             break;
 
           case "seek-completed":
@@ -73,34 +94,34 @@ export class Skin extends HTMLElement {
             break;
 
           case "rate":
-            console.log(`再生速度：${this.#player.playbackRate}`);
+            console.log(`再生速度：${gController.playbackRate}`);
             break;
 
           case "resize":
-            console.log(`解像度：${this.#player.videoWidth}x${this.#player.videoHeight}`);
+            console.log(`解像度：${gController.videoWidth}x${gController.videoHeight}`);
             break;
 
           case "dual-mono-mode":
-            console.log(`デュアルモノラル：${this.#player.dualMonoMode}`);
+            console.log(`デュアルモノラル：${gController.dualMonoMode}`);
             break;
 
           case "services":
-            console.log("全サービス更新", [...this.#player.services]);
+            console.log("全サービス更新", [...gController.services]);
             break;
 
           case "service":
-            console.log("サービス更新", this.#player.services.getById(e.serviceId));
+            console.log("サービス更新", gController.services.getById(e.serviceId));
             break;
 
           case "event": {
-            const service = this.#player.services.getById(e.serviceId);
+            const service = gController.services.getById(e.serviceId);
             const event = e.isPresent ? service.present_event : service.following_event;
             console.log(`イベント（${e.serviceId}、${e.isPresent}）`, event);
             break;
           }
 
           case "service-changed":
-            console.log(`新サービスID：${this.#player.currentServiceId}`);
+            console.log(`新サービスID：${gController.currentServiceId}`);
             break;
 
           case "stream-changed":
@@ -116,7 +137,7 @@ export class Skin extends HTMLElement {
             break;
 
           case "timestamp":
-            console.log(`日付時刻：${this.#player.timestamp}`);
+            console.log(`日付時刻：${gController.timestamp}`);
             break;
         }
         break;
